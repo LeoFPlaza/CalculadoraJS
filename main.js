a7aa062d78c3dd862b354dceeec6ae2424ba0069
LeoFPlaza <leoplaz@gmail.com>
2022-10-31 23:18:49 -0300

    agrego ciclo while para terminar la entrega

--- main.js
+++ main.js
@@ -1,34 +1,60 @@
 let firstNumber = 0;
 let lastNumber = 0;
+let operador;
 let result = document.getElementById("result");
-let userName = prompt("Nombre de usuario:"); 
+let userName = "Usuario";//prompt("Nombre de usuario:"); 
 
 
 result.innerHTML = lastNumber;
 
  console.log("==========================")
-while(userName !== "Usuario"){
-  userName = prompt("por favor ingresar un nombre de usuario valido")
+// while(userName !== "Usuario"){
+//   userName = prompt("por favor ingresar un nombre de usuario valido")
+// }
+
+
+
+function operation (operation){
+firstNumber = parseInt(lastNumber);
+lastNumber = 0;
+operador = operation;
+
+  if( isNaN(firstNumber)  || firstNumber === undefined){
+    console.log("err")
+  }
+  if (operador === "divide"|| operador === "add" || 
+  operador === "subs" || operador === "times"){
+    acButton();
+  }
 }
 
-
-
-function operation (operador)){
-let numberA = parseInt(lastNumber);
-let numberB = 0
-  if( isNaN(numberA)  || numberA === undefined){
-    console.log("err")
-  }else if (operador === "divide"){
-    acButton();
-    numberB = parseInt(lastNumber);
-      result = numberA / numberB;
-      console.log = result
+function equals(){
+  console.log(operador);
+  if(operador === "divide"){
+    lastNumber = firstNumber / parseInt(lastNumber);
+    result.innerHTML = lastNumber;
+    return(lastNumber)
+  }else  if(operador === "times"){
+    lastNumber = firstNumber * parseInt(lastNumber);
+    result.innerHTML = lastNumber;
+    return(lastNumber)
+  }else  if(operador === "add"){
+    lastNumber = firstNumber + parseInt(lastNumber);
+    result.innerHTML = lastNumber;
+    return(lastNumber)
+  }else  if(operador === "subs"){
+    lastNumber = firstNumber - parseInt(lastNumber);
+    result.innerHTML = lastNumber;
+    return(lastNumber)
+  }else{
+    result.innerHTML = lastNumber;
+    return(lastNumber)
   }
 }
 
 
 function concatNumber(numberClicked){
-  console.log(lastNumber);
+  //console.log(lastNumber);
   if(lastNumber === 0){
     lastNumber = numberClicked
     result.innerHTML = lastNumber;
@@ -36,7 +62,7 @@ function concatNumber(numberClicked){
     lastNumber = "" + lastNumber + parseInt(numberClicked);
     result.innerHTML = lastNumber;
   }
-  console.log(parseInt(lastNumber));
+  //console.log(parseInt(lastNumber));
   return(parseInt(lastNumber));
 }
 
